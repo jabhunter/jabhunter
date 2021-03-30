875520a66eb2c8b4d8c30138eddc70ecb78b0532
John <johnwcode@gmail.com>
2021-03-30 07:24:11 -0700

    make styling updates to add list

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,6 +3,7 @@ import { GoogleSheetsDbService } from 'ng-google-sheets-db';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { Location, locationAttributesMapping } from '../location.model';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -10,15 +11,26 @@ import { Location, locationAttributesMapping } from '../location.model';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  panelOpenState = false;
+  
   locations$: Observable<Location[]>;
 
-  constructor(private googleSheetsDbService: GoogleSheetsDbService) { 
+  constructor(private googleSheetsDbService: GoogleSheetsDbService, private sanitizer:DomSanitizer) { 
     this.locations$ = this.googleSheetsDbService.getActive<Location>(
       environment.locations.spreadsheetId,
       environment.locations.worksheetId,
       locationAttributesMapping,
       "Active"
     );
+  }
+
+  sanitize(url:string){
+    return this.sanitizer.bypassSecurityTrustUrl(url);
+  }
+
+  getSanitizedAddressUrl(url:string){
+    let addressUrl = `https://www.google.com/maps/place/`
+    return this.sanitizer.bypassSecurityTrustUrl(addressUrl);
   }
 
   ngOnInit(): void {
