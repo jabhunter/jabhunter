75d50378f8c086a7ad5c5865fbe1b6b0d5b1912a
John <johnwcode@gmail.com>
2021-03-31 08:31:13 -0700

    save progress

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,6 +4,9 @@ import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { Location, locationAttributesMapping } from '../location.model';
 import { DomSanitizer } from '@angular/platform-browser';
+import { from } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Pipe, PipeTransform } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -36,7 +39,37 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  public executeSelectedChange = (event: any) => {
-    console.log(event);
+  someEvent = (e: MouseEvent) => {
+    console.log(e)
+  };
+
+  showTheObj = (objToShow: object) => {
+    if (objToShow) {
+      console.log(objToShow);
+        this.objToShow.emit(this.item);
+        this.displayNone = this.displayNone ? '' : 'none';
+        this.lineThrough = this.lineThrough ? '' : 'line-through';
+      }
+      const coolLocations = objToShow.pipe
+    } else {
+      console.log("nothing to show")
+    }
   }
+
+  
+
+  public executeSelectedChange = (event: any, locations: object) => {
+    console.log(event);
+
+    console.log(locations);
+  }
+}
+
+// This component makes a request but it can't actually delete a hero.
+@Output() deleteRequest = new EventEmitter<Item>();
+
+delete() {
+  this.deleteRequest.emit(this.item);
+  this.displayNone = this.displayNone ? '' : 'none';
+  this.lineThrough = this.lineThrough ? '' : 'line-through';
 }
