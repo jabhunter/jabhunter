feb846b2334a4e9d437574316e75b0b028c0be43
John <johnwcode@gmail.com>
2021-04-01 12:02:41 -0700

    add rename covid compoenet and expand iframe driven docs to 1000vh

---
+++ src/app/renamecovid/renamecovid.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-renamecovid',
+  templateUrl: './renamecovid.component.html',
+  styleUrls: ['./renamecovid.component.scss']
+})
+export class RenameCovidComponent {
+
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
