12fc9bde7ae0739ae233c84596d234812c27f7ac
John <johnwcode@gmail.com>
2021-04-02 05:28:33 -0700

    styling updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 
 @Component({
