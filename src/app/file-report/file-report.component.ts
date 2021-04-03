2f902691db00c37ddf3e044d44193b7be9e60d37
John <johnwcode@gmail.com>
2021-04-03 00:03:17 -0700

    added file report component

---
+++ src/app/file-report/file-report.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-file-report',
+  templateUrl: './file-report.component.html',
+  styleUrls: ['./file-report.component.scss']
+})
+export class FileReportComponent {
+
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
